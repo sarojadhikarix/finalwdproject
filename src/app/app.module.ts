82069c8a96b275fd5dc11eefb36e90b38f39b073
Saroj Adhikari <sarojadhikarix@gmail.com>
2018-05-08 11:30:44 +0200

    Added controls component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BuilderComponent } from './builder/builder.component';
+import { ControlsComponent } from './controls/controls.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BuilderComponent
+    BuilderComponent,
+    ControlsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/builder/builder.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {Response} from '@angular/http';
+import {Observable} from 'rxjs';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+
+@Injectable()
+export class CommentService {
+  constructor(private http: Http) {
+  }
+
+  getControls(){
+      var controls = ['Learning Angular 2', "Pro TypeScript", "ASP.NET"]; 
+  }
+
+}
+
