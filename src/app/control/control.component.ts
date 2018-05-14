734b924a006bdffc28e8fce325863127796a21b4
Saroj Adhikari <sarojadhikarix@gmail.com>
2018-05-14 11:10:17 +0200

    changes

--- src/app/control/control.component.ts
+++ src/app/control/control.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ControlService } from './control.service';
+
 @Component({
   selector: 'app-control',
   templateUrl: './control.component.html',
-  styleUrls: ['./control.component.css']
+  styleUrls: ['./control.component.css'],
+  providers:[ControlService]
 })
 export class ControlComponent implements OnInit {
 
   constructor(private controlService : ControlService) { }
 
+  controls;
+
   ngOnInit() {
+    this.controlService.getControls().subscribe(
+      data => {
+        this.controls = data;
+        console.log(data);
+      }
   }
 
 }
--- src/app/control/control.service.ts
+++ src/app/control/control.service.ts
@@ -1,19 +1,13 @@
 import {Injectable} from '@angular/core';
-import {Response} from '@angular/http';
-import {Observable} from 'rxjs';
-import {Http} from '@angular/http';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
-
+import { AngularFireDatabase } from 'angularfire2/database-deprecated';
 
 @Injectable()
 export class ControlService{
-  constructor(private http: Http) {
+  constructor(private db: AngularFireDatabase) {
   }
 
   getControls(){
-      var controls = {"Colorways":[{"id":1,"name":"Full Bike", "image":"fullbike"},{"id":2,"name":"Framekit", "image":"framekit"},{"id":3,"name":"Steps E8000 EBullitt Full Bike"}]};
-      return controls;
+    return  this.db.list('/');
   }
 
 }
