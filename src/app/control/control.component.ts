7cfe9a174e78b8239985af87e0211b07a167d8b9
DESKTOP-FD9LD0D\n0br41n <michael4720@msn.com>
2018-05-17 09:51:11 +0200

    it works!

--- src/app/control/control.component.ts
+++ src/app/control/control.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-
 import { ControlService } from './control.service';
-
 
 @Component({
   selector: 'app-control',
@@ -11,33 +9,70 @@ import { ControlService } from './control.service';
 })
 
 
-
 export class ControlComponent implements OnInit {
 
   constructor(private controlService: ControlService) { }
   @Input() type;
-  steps = [];
-  stepstitle = [];
-  controls;
+  stepsdata:any = {};
+  stepstitle:any[] = [];
   startstep = 0;
-  endstep = 5;
   currentstep = 0;
-  
 
   ngOnInit() {
 
-    this.controlService.getControls('Bikes/' + this.type ).subscribe(
-      data => {
-        this.controls = data;
-        console.log(this.controls);
+    //const important to work async
+
+    var controlSteps = ["Colorways","Accessories","Extras"];
+    for(var i = 0; i < controlSteps.length; i++){
+
+      const name = controlSteps[i];
+
+      this.controlService.getControls(name).subscribe(
+        data => {
+
+          this.stepsdata[name] = [];
+          this.stepstitle.push(name);
+
+          for(var j = 0; j < data.length; j++){
+
+            const keys = j;
+            this.stepsdata[name].push(data[keys]);
+
+          }
       });
+    }
+
+    this.controlService.getControlsBike('Steps e8000 ebullitt full bike').subscribe(
+      data => {
+        for(var i = 0; i < data.length; i++){
+          const index = i;
+
+          this.stepstitle.push(data[index].$key);
+          const customizeName = data[index].$key;
+          this.stepsdata[customizeName] = [];
+
+          this.controlService.getControlsCustomize('Steps e8000 ebullitt full bike', customizeName).subscribe(
+            customize => {
+
+              for(var j = 0; j < customize.length; j++){
+                const k = j;
+
+                this.stepsdata[customizeName].push(customize[k]);
+                
+              }
+          });
+        }
+      });
+
   }
 
   next() {
-    if (this.currentstep < 5) {
+    if (this.currentstep < this.stepstitle.length - 1) {
       this.currentstep = this.currentstep + 1;
-    } else if (this.currentstep == 5) {
-      document.getElementById('nextbutton').innerText = "Finish and Go to cart"
+      console.log(this.stepstitle);
+    }
+    else if (this.currentstep == this.stepstitle.length - 1) {
+      document.getElementById('nextbutton').innerText = "Finish and Go to cart";
     }
   }
 
